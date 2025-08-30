1461a223353a19cb738ff4d67a51e10ed967d42e
Ahmed Medhat <ahmed.medhat.2029@gmail.com>
2025-08-30 14:31:17 +0300

    Add project files.

---
+++ Singleton/Logger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Singleton;
+
+class Logger
+{
+	// Not thread-safe implementation
+	//private static Logger _logger;
+
+	//public static Logger Instance
+	//{
+	//	get
+	//	{
+	//		if (_logger is null)
+	//		{
+	//			_logger = new Logger();
+	//		}
+
+	//		return _logger;
+	//	}
+	//}
+
+	// Thread-safe implementation
+	private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+	public static Logger Instance
+	{
+		get
+		{
+			return _lazyLogger.Value;
+		}
+	}
+
+	protected Logger()
+	{
+
+	}
+
+	public void Log(string message)
+	{
+		Console.WriteLine($"Message to log: {message}");
+	}
+}
